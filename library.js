25a2c662b0008a15594fedd0ae1903f8f2c7bc81
edwards955 <getignorantfriday@gmail.com>
2023-08-11 22:38:13 -0400

    Add Book Constructor and new book function to script

---
+++ library.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => `${title} by ${author}, ${pages} pages, ${read}`;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
